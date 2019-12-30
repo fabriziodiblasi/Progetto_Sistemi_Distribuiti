23df67dadd007fd24a6c7d33bc4bd0a9fc6abeba
fabrizio <fabriziodiblasi92@gmail.com>
2019-12-30 23:43:11 +0100

    hello world java

---
+++ Java_Project/src/Prova_Seriale.java
@@ -0,0 +1,5 @@
+public class Prova_Seriale {
+    public static void main(String[] args) {
+        System.out.println("Ciao!!");
+    }
+}
--- sketch_arduino/sensor_sketch/sensor_sketch.ino
+++ sketch_arduino/sensor_sketch/sensor_sketch.ino
@@ -1,6 +1,15 @@
 float sensor_dx=A0;
 float sensor_sx=A3;
 float gas_value;
+char buffer[8];
+
+void scrivi_seriale(char id_sens, int value){
+  sprintf(buffer,"%c%d%c",id_sens,value,'_');
+  //Serial.print(buffer);
+  Serial.write(buffer,sizeof(char)*8);
+  buffer[0]='\0';
+}
+
 void setup()
 {
   pinMode(sensor_dx,INPUT);
@@ -12,9 +21,9 @@ void setup()
 void loop()
 {
   gas_value=analogRead(sensor_dx);
-  Serial.print("DX_");Serial.println(gas_value);
+  scrivi_seriale('D',gas_value);
   delay(2000);
   gas_value=analogRead(sensor_sx);
-  Serial.print("SX_");Serial.println(gas_value);
+  scrivi_seriale('S',gas_value);
   delay(2000);
 }
